26b74b833f126bc2261f49c4f69d826652d6d723
sinsky <wesky93@gmail.com>
2020-04-11 17:17:14 +0900

    add asnyc server result

--- src/hellow_server/sync_server.py
+++ src/hellow_server/sync_server.py
@@ -14,7 +14,7 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
